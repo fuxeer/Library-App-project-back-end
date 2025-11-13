d8666e0ca44f9004b7b30a6a4c11df30f36efa7b
fuxeer <faisal0500656512@gmail.com>
2025-11-13 20:06:40 +0300

    deleted weatherforcast test

--- obj/Debug/net8.0/App_library_back-end.AssemblyInfo.cs
+++ obj/Debug/net8.0/App_library_back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App_library_back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6fbbcc4e4f631c95df9079c5d23cf37a3273f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("App_library_back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App_library_back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
