e0d8f4813181bf58a8f98a45a8f6e8df3b76aca5
baderbahakeem-1421 <baderbahakeem1421@gmail.com>
2025-11-14 17:22:36 +0300

    Bader commit

--- App_library_back-end/Model/User.cs
+++ App_library_back-end/Model/User.cs
@@ -3,12 +3,16 @@ namespace App_library_back_end.Model
 {
     public class User
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
         public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string UserName { get; set; } =string.Empty;
+        public string UserName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string? Email { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
         public string? PhoneNo { get; set; }
+        public string? Address { get; set; }
+        public char? UserType { get; set; }
         public string? Type { get; set; }
 
       
